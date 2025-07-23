779653fbd0b94538907070fbf7aab2019bf0213f
nferno55 <jerry.eveland@gmail.com>
2025-07-23 14:36:13 -0500

    Duplicate check
    added to the script to look for duplicates and list them

--- data_checks.py
+++ data_checks.py
@@ -16,9 +16,10 @@ queries = (
     "SELECT * FROM mock_data WHERE gender IS NULL;",
     "SELECT * FROM mock_data WHERE phone_number IS NULL;",
     "SELECT * FROM mock_data WHERE account IS NULL;",
-    "SELECT * FROM mock_data WHERE date_created IS NULL;"
+    "SELECT * FROM mock_data WHERE date_created IS NULL;",
 )
 
+# looking for NULL values
 for query in queries:
     print(f"Running current query; {query}")
     # Read into a DataFrame
@@ -37,3 +38,18 @@ for query in queries:
     # Save to CSV
     df.to_csv(filename, index=False)
     print(f"Exported to {filename}\n")
+
+# list of possible duplicate fields
+values = ("first_name", "last_name", "email", "account", "phone_number")
+
+for column in values:
+    query = f"""
+        SELECT {column}, COUNT(*) as count
+        FROM mock_data
+        WHERE {column} IS NOT NULL
+        GROUP BY {column}
+        HAVING count > 1;
+    """
+    df = pd.read_sql_query(query, conn)
+    df.to_csv(f"duplicate_{column}.csv", index=False)
+    print(f"Exported duplicate check for {column}")
