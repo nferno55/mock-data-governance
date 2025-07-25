9bcdb0b6cd62b93f0359467c8bf5a0de4008ba2e
nferno55 <jerry.eveland@gmail.com>
2025-07-25 07:57:54 -0500

    Update data_checks.py

--- data_checks.py
+++ data_checks.py
@@ -7,20 +7,9 @@ import os
 # Connect to SQLite database
 conn = sqlite3.connect("mock_data.db")
 
-# list of possible duplicate fields
-all_values = ("first_name", "last_name", "email", "account", "phone_number")
+# list of all fields
+all_values = ("first_name", "last_name", "email", "gender", "account", "phone_number", "date_created")
 
-# Create a list of queries that will run automatically and output their results into CSV files
-# List of queries to run
-queries = (
-    "SELECT * FROM mock_data WHERE first_name IS NULL;",
-    "SELECT * FROM mock_data WHERE last_name IS NULL;",
-    "SELECT * FROM mock_data WHERE email IS NULL;",
-    "SELECT * FROM mock_data WHERE gender IS NULL;",
-    "SELECT * FROM mock_data WHERE phone_number IS NULL;",
-    "SELECT * FROM mock_data WHERE account IS NULL;",
-    "SELECT * FROM mock_data WHERE date_created IS NULL;",
-)
 
 # looking for NULL values
 for text in all_values:
@@ -34,6 +23,7 @@ for text in all_values:
 # list of possible duplicate fields
 values = ("first_name", "last_name", "email", "account", "phone_number")
 
+# looking for DUPLICATES, ignoring gender and date_created
 for column in values:
     query = f"""
         SELECT {column}, COUNT(*) as count
