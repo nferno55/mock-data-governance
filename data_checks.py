578567f45732f09eed2669cf2482a30d1de056d1
nferno55 <jerry.eveland@gmail.com>
2025-08-01 12:05:06 -0500

    Update data_checks.py
    slight modifications to the python file

--- data_checks.py
+++ data_checks.py
@@ -17,7 +17,7 @@ conn = sqlite3.connect(db_name)
 total_entries: int
 # list of all fields
 all_values = ("first_name", "last_name", "email", "gender", "account", "phone_number", "date_created")
-# list of possible duplicate fields
+# list of possible duplicate fields; some would not be used with real-world data obviously
 values = ("first_name", "last_name", "email", "account", "phone_number")
 # used to store all values where there are no issues, so we do not print out blank CSV files
 no_issues = []
